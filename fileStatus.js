71ed6ee882d277299d90bec2e96f2712a0b368a4
marianalopesdev <marianalopes.dev@gmail.com>
2023-04-23 05:06:33 -0300

    getting status from file links using https

--- fileStatus.js
+++ fileStatus.js
@@ -12,8 +12,21 @@ module.exports = function printMsg() {
     const linksRegex = /(https?:\/\/\S+(?=\b))/g;
 
     const links = contents.toString().match(linksRegex);
-   
-   console.log(links);    
+  
+    for (let i = 0; i <= links.length - 1; i++) {
+      
+      let link = links[i];
+      // console.log([i] +" "+ link);
+
+      https.get(link, (response) => {
+        response.setEncoding("utf8");
+        response.on("error", console.error);
+      
+
+        
+        console.log(`Link: ${link} - ${response.statusCode}`);
+      });
+    }   
 
     console.log(`The file name entered at the prompt is ${file}`);
   });
