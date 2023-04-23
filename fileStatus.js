a289759d4b19507011f72b00886e7dcf790de829
marianalopesdev <marianalopes.dev@gmail.com>
2023-04-23 06:24:20 -0300

    handling status message / chalk added

--- fileStatus.js
+++ fileStatus.js
@@ -1,8 +1,12 @@
+const showHttpStatusMessages = require("./httpStatusMessages");
+const chalk = require("chalk");
 let https = require("https");
 const fs = require("fs");
 
 module.exports = function printMsg() {
   const file = process.argv[2];
+
+  console.log(chalk.blue("Hello world!"));
 
   fs.readFile(file, function (err, contents) {
     if (err) {
@@ -12,22 +16,19 @@ module.exports = function printMsg() {
     const linksRegex = /(https?:\/\/\S+(?=\b))/g;
 
     const links = contents.toString().match(linksRegex);
-  
     for (let i = 0; i <= links.length - 1; i++) {
-      
       let link = links[i];
       // console.log([i] +" "+ link);
 
       https.get(link, (response) => {
         response.setEncoding("utf8");
         response.on("error", console.error);
-      
+        const httpStatusMessage = showHttpStatusMessages(response.statusCode);
 
-        
-        console.log(`Link: ${link} - ${response.statusCode}`);
+        console.log(`Link: ${link} - ${httpStatusMessage}`);
       });
-    }   
+    }
 
-    console.log(`The file name entered at the prompt is ${file}`);
+    console.log(`O arquivo ${file} contém ${links.length - 1}links.`);
   });
 };
