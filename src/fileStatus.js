c77f7fb8a206d50ec91629caa94b872b52e707a0
marianalopesdev <marianalopes.dev@gmail.com>
2023-05-03 05:54:52 -0300

    first steps and organization - object created

--- src/fileStatus.js
+++ src/fileStatus.js
@@ -5,21 +5,43 @@ const linkExtractor = require("./linkExtractor");
 const validateLinks = require("./validateLink");
 const chalk = require("chalk");
 const path = require("path");
+const { table } = require("console");
 
 module.exports = function mdLinks(typedPath, option) {
   const opt = option;
   const filePath = typedPath;
   const fileName = path.basename(filePath);
 console.log(opt);
-if (!opt.validate && !opt.stats) {
+
+
+const lala = (fileContents) => {
+
+  const {obj} = linkExtractor(fileContents);
+
+//  console.log(obj);
+  if (!opt.validate && !opt.stats) {
+   // console.log(obj);
+  } 
+  else if (opt.validate && !opt.stats) {
+
+  }
+
+}
+
+
+
+// if (!opt.validate && !opt.stats) {
     fileReader(filePath)
       .then((fileContents) => {
-        let { table } = linkExtractor(fileContents);
-        console.log(`The file ${fileName} contains ${table.length - 1} links.`);
-        console.log(table.toString());
+        lala(fileContents);
+        // let { table } = linkExtractor(fileContents);
+        // console.log(`The file ${fileName} contains ${table.length - 1} links.`);
+        // console.log(table.toString());
       })
       .catch((error) => {
-        errorHandling(error.code);
+        const error1 = new Error('hshs');
+        error1.code = "NO_LINKS";
+        errorHandling(error1.code);
         // readDir(filePath)
         //   .then((dirContent) => {
         //     console.log(dirContent);
@@ -30,42 +52,42 @@ if (!opt.validate && !opt.stats) {
         //     errorHandling(error.code);
         //   });
       });
-  } else if (opt.validate && !opt.stats) {
-    fileReader(filePath)
-      .then((fileContents) => {
-        let { links } = linkExtractor(fileContents);
-        validateLinks(links)
-          .then((table) => {
-            console.log(table.toString());
-          })
-          .catch((error) => {
-            errorHandling(error.code);
-          });
-      })
-      .catch((error) => {
-        errorHandling(error.code);
-      });
-  } else if (!opt.validate && opt.stats) {
-    fileReader(filePath)
-      .then((fileContents) => {
-        let { links } = linkExtractor(fileContents);
-        linkStats(links);
-      })
-      .catch((error) => {
-        errorHandling(error.code);
-      });
-  } else {
-    fileReader(filePath)
-      .then((fileContents) => {
-        let { links } = linkExtractor(fileContents);
-        let { uniqueLinks } = linkStats(links);
-        let { brokenLsinks } = validateLinks(uniqueLinks).then((brokenLinks) => {
+  // } else if (opt.validate && !opt.stats) {
+  //   fileReader(filePath)
+  //     .then((fileContents) => {
+  //       let { links } = linkExtractor(fileContents);
+  //       validateLinks(links)
+  //         .then((table) => {
+  //           console.log(table.toString());
+  //         })
+  //         .catch((error) => {
+  //           errorHandling(error.code);
+  //         });
+  //     })
+  //     .catch((error) => {
+  //       errorHandling(error.code);
+  //     });
+  // } else if (!opt.validate && opt.stats) {
+  //   fileReader(filePath)
+  //     .then((fileContents) => {
+  //       let { links } = linkExtractor(fileContents);
+  //       linkStats(links);
+  //     })
+  //     .catch((error) => {
+  //       errorHandling(error.code);
+  //     });
+  // } else {
+  //   fileReader(filePath)
+  //     .then((fileContents) => {
+  //       let { links } = linkExtractor(fileContents);
+  //       let { uniqueLinks } = linkStats(links);
+  //       let { brokenLsinks } = validateLinks(uniqueLinks).then((brokenLinks) => {
           
-          console.log("Broken Links: " + brokenLsinks);
-        });
-      })
-      .catch((error) => {
-        errorHandling(error.code);
-      });
-  }
+  //         console.log("Broken Links: " + brokenLsinks);
+  //       });
+  //     })
+  //     .catch((error) => {
+  //       errorHandling(error.code);
+  //     });
+  // }
 };
--- src/linkExtractor.js
+++ src/linkExtractor.js
@@ -1,5 +1,21 @@
 const Table = require("cli-table");
 const errorHandling = require("./errorHandling");
+//se eu usar a flag, posso montar as 3 table no link extractor
+//o validate pode funcionar aqui table//inclusive faz mais sentido
+//o stats tb
+//faz bem mais sentido
+//o if else ficaria aquit b
+//retornaria o obj links validados, stats de links e etc
+
+//momento2
+//criei um objeto, jogeui ele na tabela
+//tudo ok
+
+//continuar daí
+//no momento filestatus nao tah fazedno if
+//trocar nomes variaveis
+
+
 
 module.exports = function linkExtractor(fileContents, flag) {
   const httpLinksRegex = /(https?:\/\/\S+(?=\b))/gm;
@@ -10,8 +26,9 @@ module.exports = function linkExtractor(fileContents, flag) {
     linkText.replace(/\[|\]/g, "")
   );
 
+ // console.log(links);
   if (links === null) {
-    const error = new Error();
+    const error = new Error('No links in the file');
     error.code = "NO_LINKS";
     return errorHandling(error.code);
   }
@@ -22,8 +39,19 @@ module.exports = function linkExtractor(fileContents, flag) {
   });
 
   for (let i = 0; i <= links.length - 1; i++) {
-    table.push([links[i], linkTextsWithoutBrackets[i], "filePath"]);
+   // table.push([links[i], linkTextsWithoutBrackets[i], "filePath"]);
+   const  obj = {
+      link: links[i],
+      text: linkTextsWithoutBrackets[i], 
+      path: 'path'
+    };
+    table.push([obj.link, obj.text, obj.path]);
+   console.log(obj);
+ 
   }
+  console.log(table.toString());
+  return table;
 
-  return { links, linkTextsWithoutBrackets, table };
+
+  
 };
